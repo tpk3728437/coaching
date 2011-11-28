d2b61bb68e4a91cd767699dfe3cf32ff7a6ad469
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2011-11-28 14:56:19 +0200

    fine pruning

--- team1/main.cpp
+++ team1/main.cpp
@@ -3,8 +3,6 @@
 
 int main(int /*argc*/, char */*argv*/[])
 {
-    std::cout << "jee" << std::endl;
-
     std::cout << "1 plus 2 is " << calculator::plus(1,2) << std::endl;
 
     std::cout << "2 minus 1 is " << calculator::minus(2,1) << std::endl;
