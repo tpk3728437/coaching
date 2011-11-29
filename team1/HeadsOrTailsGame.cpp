5e84799e9821e934bcd9f46af3a16217e8aa0cb7
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2011-11-29 14:11:48 +0200

    real gameplay

--- team1/HeadsOrTailsGame.cpp
+++ team1/HeadsOrTailsGame.cpp
@@ -1,4 +1,6 @@
 #include "HeadsOrTailsGame.h"
+#include <time.h>
+#include <stdlib.h>
 
 HeadsOrTailsGame::HeadsOrTailsGame()
 {
@@ -10,6 +12,12 @@ HeadsOrTailsGame::~HeadsOrTailsGame()
 
 HeadsOrTailsGame::Side HeadsOrTailsGame::Play()
 {
-    return Tails;
+    /* initialize random seed: */
+    srand ( time(0) );
+
+    /* generate secret number: */
+    int result = rand() % 2;
+
+    return (HeadsOrTailsGame::Side) result;
 }
 
