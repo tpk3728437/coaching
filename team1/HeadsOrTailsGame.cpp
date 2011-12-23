c0345cc8d5694ed2f20835aea89388f468f970f9
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2011-12-23 10:59:55 +0200

    second test case

--- team1/HeadsOrTailsGame.cpp
+++ team1/HeadsOrTailsGame.cpp
@@ -17,6 +17,7 @@ HeadsOrTailsGame::~HeadsOrTailsGame()
 
 void HeadsOrTailsGame::Play()
 {
+    int numberOfHeads = 0;
     // signal player onPlayStarted()
     m_player.onPlayStarted();
 
@@ -24,10 +25,24 @@ void HeadsOrTailsGame::Play()
     // state machine
     // flip one coin, then signal player that coin has been flipped with result
     const Side firstResult = (Side) m_gamePlay.Flip();
+    if (firstResult == Heads) 
+    {
+        ++numberOfHeads;
+    }
     m_player.onCoinFlipped(0, firstResult);
 
     const Side secondResult = (Side) m_gamePlay.Flip();
+    if (secondResult == Heads) 
+    {
+        ++numberOfHeads;
+    }
     m_player.onCoinFlipped(1, secondResult);
+
+    if (numberOfHeads == 1)
+    {
+        const Side thirdResult = (Side) m_gamePlay.Flip();
+        m_player.onCoinFlipped(2, thirdResult);
+    }
 
     // signal player Game Loss
     m_player.onGameLoss();
--- team1/test.cpp
+++ team1/test.cpp
@@ -17,7 +17,7 @@ TEST(FlipCoin, lose_with_two_tails)
 
     MockPlayer player;
     EXPECT_CALL(player, onPlayStarted());
-    EXPECT_CALL(player, onCoinFlipped(Eq(0), Heads));
+    EXPECT_CALL(player, onCoinFlipped(Eq(0), Tails));
     EXPECT_CALL(player, onCoinFlipped(Eq(1), Tails));
     EXPECT_CALL(player, onGameWin()).Times(0);
     EXPECT_CALL(player, onGameLoss());
@@ -26,3 +26,24 @@ TEST(FlipCoin, lose_with_two_tails)
     HeadsOrTailsGame game(play, player);
     game.Play();    
 }
+
+TEST(FlipCoin, lose_with_heads_then_two_tails)
+{
+    MockGamePlay play;
+    EXPECT_CALL(play, Flip()).Times(3)
+        .WillOnce(Return(Heads))
+        .WillRepeatedly(Return(Tails));
+
+    MockPlayer player;
+    EXPECT_CALL(player, onPlayStarted());
+    EXPECT_CALL(player, onCoinFlipped(Eq(0), Heads));
+    EXPECT_CALL(player, onCoinFlipped(Eq(1), Tails));
+    EXPECT_CALL(player, onCoinFlipped(Eq(2), Tails));
+    EXPECT_CALL(player, onGameWin()).Times(0);
+    EXPECT_CALL(player, onGameLoss());
+    EXPECT_CALL(player, onGameEnd());
+
+    HeadsOrTailsGame game(play, player);
+    game.Play();    
+}
+
